3f69d8c1f9e33a616254f457b09ca9b6642f5c3e
Mihir0106 <mjmihir9617@gmail.com>
2021-01-22 15:22:26 +0530

    Upload

---
+++ Assets/AR content/AR script/ARTaptoPlaceObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTaptoPlaceObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject gameObjectToInstantiate;
+
+    private GameObject spawnedObject;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(index: 0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitpose = hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitpose.position, hitpose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitpose.position;
+            }
+        }
+    }
+     
+    // Start is called before the first frame update
+    public void Disable()
+    {
+        GetComponent<ARTaptoPlaceObject>().enabled = false;
+    }
+
+    public void able()
+    {
+        GetComponent<ARTaptoPlaceObject>().enabled = true;
+    }
+
+}
---
+++ Assets/Scripts/disablePlane.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disablePlane : MonoBehaviour
+{
+    public GameObject Manager;
+    // Start is called before the first frame update
+    public void Disable()
+    {
+        Manager.SetActive(false);
+    }
+    public void able()
+    {
+        Manager.SetActive(true);
+    }
+
+
+}
